460956314d575ab4967043112c22c66b2a0ff054
youth.s <youth.s@youthsdeMacBook-Pro.local>
2018-12-31 22:09:38 +0800

    访问修饰符

--- Scala/HelloWorld.scala
+++ Scala/HelloWorld.scala
@@ -1,26 +1,12 @@
 object HelloWorld {
-   /* 这是我的第一个 Scala 程序
-    * 以下程序将输出'Hello World!' 
-    */
-   def main(args: Array[String]) {
-      println("Hello, world!") // 输出 Hello World
+   class Outer {
+   class Inner {
+      def f() { println("f") }
+      class InnerMost {
+         f() // 正确
+      }
    }
+   (new Inner).f() // 正确因为 f() 是 public
 }
-
-定义多行字符串
-val foo = """1
-     | 2
-     | 3
-     | 4
-     | 6"""
-
-变量声明
-var myVar : String = "Foo"
-
-常量声明
-val myVal : String = "Foo"
-
-多个变量声明
-var xmax, ymax = 100  // xmax, ymax都声明为100
-
+}
 
