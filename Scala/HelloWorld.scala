a3475751b061bc1e356b67936db97e417d9379b0
youth.s <youth.s@youthsdeMacBook-Pro.local>
2019-01-01 14:04:04 +0800

    正则表达式

--- Scala/HelloWorld.scala
+++ Scala/HelloWorld.scala
@@ -1,15 +1,8 @@
+import scala.util.matching.Regex
 object HelloWorld {
-     def matchTest(x: Any): Any = x match {
-      case 1 => "one"
-      case "two" => 2
-      case y: Int => "scala.Int"
-      case _ => "many"
-   }
-
     def main(args: Array[String]) {
-      println(matchTest("two"))
-      println(matchTest("test"))
-      println(matchTest(1))
-      println(matchTest(6))
+      val pattern = new Regex("(S|s)cala")  // 首字母可以是大写 S 或小写 s
+      val str = "Scala is scalable and cool"
+      println((pattern findAllIn str).mkString(","))   // 使用逗号 , 连接返回结果
    }
 }
