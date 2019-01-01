87f8e19a327ba3f803c1fed469523e3c7ca7fff4
youth.s <youth.s@youthsdeMacBook-Pro.local>
2019-01-01 13:55:26 +0800

    匹配模式

--- Scala/HelloWorld.scala
+++ Scala/HelloWorld.scala
@@ -1,23 +1,15 @@
 object HelloWorld {
-    trait Equal {
-      def isEqual(x: Any): Boolean
-      def isNotEqual(x: Any): Boolean = !isEqual(x)
-    }
-
-    class Point(xc: Int, yc: Int) extends Equal {
-      var x: Int = xc
-      var y: Int = yc
-      def isEqual(obj: Any) =
-        obj.isInstanceOf[Point] &&
-        obj.asInstanceOf[Point].x == x
-    }
+     def matchTest(x: Any): Any = x match {
+      case 1 => "one"
+      case "two" => 2
+      case y: Int => "scala.Int"
+      case _ => "many"
+   }
 
     def main(args: Array[String]) {
-      val p1 = new Point(2, 3)
-      val p2 = new Point(2, 4)
-      val p3 = new Point(3, 3)
-      println(p1.isNotEqual(p2))
-      println(p1.isNotEqual(p3))
-      println(p1.isNotEqual(2))
+      println(matchTest("two"))
+      println(matchTest("test"))
+      println(matchTest(1))
+      println(matchTest(6))
    }
 }
