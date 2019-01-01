a4d83b78d233d5447b6c289b2e183aa1a1900a1b
youth.s <youth.s@youthsdeMacBook-Pro.local>
2019-01-01 12:42:28 +0800

    集合

--- Scala/HelloWorld.scala
+++ Scala/HelloWorld.scala
@@ -1,16 +1,8 @@
-import Array._
 object HelloWorld {
     def main(args: Array[String]) {
-       var myList1 = range(10, 20, 2)
-      var myList2 = range(10,20)
-
-      // 输出所有数组元素
-      for ( x <- myList1 ) {
-         print( " " + x )
-      }
-      println()
-      for ( x <- myList2 ) {
-         print( " " + x )
+      val it = Iterator("Baidu", "Google", "Runoob", "Taobao")
+      while (it.hasNext){
+         println(it.next())
       }
    }
 }
