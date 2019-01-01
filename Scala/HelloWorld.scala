140488e834da87ab3fee315efa6b56a33f4b17fb
youth.s <youth.s@youthsdeMacBook-Pro.local>
2019-01-01 13:48:17 +0800

    Trait(特征)

--- Scala/HelloWorld.scala
+++ Scala/HelloWorld.scala
@@ -1,32 +1,23 @@
-// 私有构造方法
-class Marker private(val color:String) {
+object HelloWorld {
+    trait Equal {
+      def isEqual(x: Any): Boolean
+      def isNotEqual(x: Any): Boolean = !isEqual(x)
+    }
 
-  println("创建" + this)
-  
-  override def toString(): String = "颜色标记："+ color
-  
-}
+    class Point(xc: Int, yc: Int) extends Equal {
+      var x: Int = xc
+      var y: Int = yc
+      def isEqual(obj: Any) =
+        obj.isInstanceOf[Point] &&
+        obj.asInstanceOf[Point].x == x
+    }
 
-// 伴生对象，与类共享名字，可以访问类的私有属性和方法
-object Marker{
-  
-    private val markers: Map[String, Marker] = Map(
-      "red" -> new Marker("red"),
-      "blue" -> new Marker("blue"),
-      "green" -> new Marker("green")
-    )
-    
-    def apply(color:String) = {
-      if(markers.contains(color)) markers(color) else null
-    }
-  
-    
-    def getMarker(color:String) = { 
-      if(markers.contains(color)) markers(color) else null
-    }
-    def main(args: Array[String]) { 
-        println(Marker("red"))  
-        // 单例函数调用，省略了.(点)符号  
-        println(Marker getMarker "blue")  
-    }
+    def main(args: Array[String]) {
+      val p1 = new Point(2, 3)
+      val p2 = new Point(2, 4)
+      val p3 = new Point(3, 3)
+      println(p1.isNotEqual(p2))
+      println(p1.isNotEqual(p3))
+      println(p1.isNotEqual(2))
+   }
 }
