5d0e14544aec3bc77a69cee18e69378887ded309
严帅 <shuai.yan@zatech.com>
2019-04-24 22:07:41 +0800

    itoken first commit

---
+++ itoken/yan-zipkin/src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/home")
+    public String index(){
+        return "up zipkin";
+    }
+}
