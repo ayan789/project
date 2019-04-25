902f606c0a6c329abd7c8a66c3a04e1b46cc365a
严帅 <shuai.yan@zatech.com>
2019-04-25 15:57:11 +0800

    springCloud config serviceId bootstrap.properties

--- itoken/yan-config/src/main/java/com/example/demo/IndexController.java
+++ itoken/yan-config/src/main/java/com/example/demo/IndexController.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableEurekaClient
 public class IndexController {
     @GetMapping("/home")
     public String index(){
