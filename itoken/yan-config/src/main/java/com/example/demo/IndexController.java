6a8c13af67d1fb45e01c0292f423513a86d81fc7
严帅 <shuai.yan@zatech.com>
2019-04-25 11:03:11 +0800

    dependencies项目依赖

--- itoken/yan-config/src/main/java/com/example/demo/IndexController.java
+++ itoken/yan-config/src/main/java/com/example/demo/IndexController.java
@@ -11,4 +11,10 @@ public class IndexController {
     public String index(){
         return "up config";
     }
+
+    @GetMapping("/desc")
+    public void desc(){
+        DemoUtils desc = new DemoUtils();
+        desc.common();
+    }
 }
---
+++ itoken/yan-eureka/src/main/java/com/example/demo/DemoUtils.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class DemoUtils {
+    public void  common(){
+        System.out.println("common");
+    }
+}
