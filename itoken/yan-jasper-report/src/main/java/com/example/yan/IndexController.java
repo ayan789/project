47d042bd47371cf2154754000e5d14c9fdeab649
yan shuai <704627050@qq.com>
2019-07-15 13:01:54 +0800

    pdf

--- itoken/yan-jasper-report/src/main/java/com/example/yan/IndexController.java
+++ itoken/yan-jasper-report/src/main/java/com/example/yan/IndexController.java
@@ -1,15 +1,22 @@
 package com.example.yan;
 
 
+import net.sf.jasperreports.engine.JRParameter;
 import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.json.JRJsonNode;
+import net.sf.jasperreports.engine.query.JsonQueryExecuterFactory;
+import net.sf.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServlet;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 @RestController
@@ -23,8 +30,8 @@ public class IndexController extends HttpServlet {
     public void pdf(){
         try
         {
-            File reportFile = new File("/Users/yanshuai/Downloads/BOOT-INF/report3.jasper");
-            File reportPdfFile = new File("/Users/yanshuai/Downloads/BOOT-INF/report3.pdf");
+            File reportFile = new File("/Users/yanshuai/Downloads/BOOT-INF/report6.jasper");
+            File reportPdfFile = new File("/Users/yanshuai/Downloads/BOOT-INF/report6.pdf");
             Map<String,Object> parameters=new HashMap<String,Object>();
             JRBeanCollectionDataSource resultsList = new JRBeanCollectionDataSource(UserSvi.createUsers());
             parameters.put("resultsList", resultsList);
@@ -43,5 +50,45 @@ public class IndexController extends HttpServlet {
             e.printStackTrace();
         }
     }
+
+
+    @GetMapping("/pdf2")
+    public void pdf2(){
+        try
+        {
+            File reportFile = new File("/Users/yanshuai/Downloads/BOOT-INF/report12.jasper");
+            File reportPdfFile = new File("/Users/yanshuai/Downloads/BOOT-INF/report12.pdf");
+            Map<String,Object> parameters=new HashMap<String,Object>();
+            Data data1 = new Data();
+            data1.setAa("www");
+            data1.setBb("rrr");
+
+            JSONObject jsonObject = JSONObject.fromObject(data1);
+            String json = jsonObject.toString();
+
+            Map<String, Object> jasperPara = new HashMap<String, Object>();
+
+            InputStream is = new ByteArrayInputStream(json.getBytes());
+            jasperPara.put("data", is);
+            jasperPara.put(JsonQueryExecuterFactory.JSON_LOCALE, Locale.CHINA);
+            jasperPara.put(JRParameter.REPORT_LOCALE, Locale.CHINA);
+
+            try {
+                byte[] reportStream = JasperRunManager.runReportToPdf(reportFile.getPath(),jasperPara);
+                FileOutputStream fw = new FileOutputStream(reportPdfFile);
+                fw.write(reportStream);
+                fw.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
--- itoken/yan-jasper-report/src/main/java/com/example/yan/UserSvi.java
+++ itoken/yan-jasper-report/src/main/java/com/example/yan/UserSvi.java
@@ -8,8 +8,13 @@ public class UserSvi {
         List<User> list = new ArrayList<User>();
         User user = new User();
         user.setSalt("hello jasper report");
-        user.setPassword("aaa");
+        user.setPassword("多哈我会对萨哈嗲和滴哦啊基督教");
+
+        User user1 = new User();
+        user1.setSalt("我的借口来我家啊德克勒克");
+        user1.setPassword("拉DSL卡桑德拉来了");
         list.add(user);
+        list.add(user1);
         return list;
     }
 
