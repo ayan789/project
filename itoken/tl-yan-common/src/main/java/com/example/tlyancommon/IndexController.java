ad01a30d22029c563910ceb17e550b4821a7247e
yan shuai <704627050@qq.com>
2019-07-22 15:48:38 +0800

    xls 导出

--- itoken/tl-yan-common/src/main/java/com/example/tlyancommon/IndexController.java
+++ itoken/tl-yan-common/src/main/java/com/example/tlyancommon/IndexController.java
@@ -7,15 +7,18 @@ import com.alibaba.excel.util.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.net.URL;
-import java.util.List;
-import java.util.Properties;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.metadata.Table;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @RestController
@@ -148,4 +151,50 @@ public class IndexController {
 
         return "OK";
     }
+
+    @GetMapping("/down")
+    public void cooperation(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ServletOutputStream out = response.getOutputStream();
+        response.setContentType("multipart/form-data");
+        response.setCharacterEncoding("utf-8");
+        String fileName = new String(("UserInfo " + new SimpleDateFormat("yyyy-MM-dd").format(new Date()))
+        .getBytes(), "UTF-8");
+        response.setHeader("Content-disposition", "attachment;filename="+fileName+".xlsx");
+        ExcelWriter writer = EasyExcelFactory.getWriter(out);
+
+        //写第一个sheet, sheet1  数据全是List<String> 无模型映射关系
+        Sheet sheet1 = new Sheet(1, 3);
+        sheet1.setSheetName("第一个sheet");
+        //设置列宽 设置每列的宽度
+        Map columnWidth = new HashMap();
+        columnWidth.put(0,10000);columnWidth.put(1,40000);columnWidth.put(2,10000);columnWidth.put(3,10000);
+        sheet1.setColumnWidthMap(columnWidth);
+        sheet1.setHead(DataUtil.createTestListStringHead());
+        //or 设置自适应宽度
+        //sheet1.setAutoWidth(Boolean.TRUE);
+        writer.write1(DataUtil.createTestListObject(), sheet1);
+
+        //写第二个sheet sheet2  模型上打有表头的注解，合并单元格
+        Sheet sheet2 = new Sheet(2, 3, WriteModel.class, "第二个sheet", null);
+        sheet2.setTableStyle(DataUtil.createTableStyle());
+        writer.write(DataUtil.createTestListJavaMode(), sheet2);
+
+        //写第三个sheet包含多个table情况
+        Sheet sheet3 = new Sheet(3, 0);
+        sheet3.setSheetName("第三个sheet");
+        Table table1 = new Table(1);
+        table1.setHead(DataUtil.createTestListStringHead());
+        writer.write1(DataUtil.createTestListObject(), sheet3, table1);
+
+        //写sheet2  模型上打有表头的注解
+        Table table2 = new Table(2);
+        table2.setTableStyle(DataUtil.createTableStyle());
+        table2.setClazz(WriteModel.class);
+        writer.write(DataUtil.createTestListJavaMode(), sheet3, table2);
+
+        //关闭资源
+        writer.finish();
+        out.close();
+    }
+
 }
