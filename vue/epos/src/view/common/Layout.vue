12e599dee8d9091bcb106d907eb1a1d10f60b912
yan shuai <704627050@qq.com>
2019-08-15 19:26:55 +0800

    vue elemeui

--- vue/epos/src/view/common/Layout.vue
+++ vue/epos/src/view/common/Layout.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="layout">
-  jjj
+  <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="3">
+            <a href="https://www.ele.me" target="_blank">订单管理</a>
+        </el-menu-item>
+    </el-menu>
   <!-- 页面注入区 -->
   <div style=" padding:300px; background-color: #f0f0f0; ">
      <router-view/>
@@ -13,9 +24,16 @@ export default {
   name: 'Layout',
   data () {
     return {
-      msg: 'Layout'
+      msg: 'Layout',
+       activeIndex: '1',
+       activeIndex2: '1'
     }
-  }
+  },
+  methods: {
+            handleSelect(key, keyPath) {
+                console.log(key, keyPath);
+            }
+        }
 }
 </script>
 
