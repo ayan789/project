580acb1754493fdd5c0795539b394da3f2ef78f3
Youths <704627050@qq.com>
2020-09-04 11:26:21 +0800

    公共组件

---
+++ java-pro/tl-common-components/src/main/java/com/example/tlcommoncomponents/TlCommonComponentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tlcommoncomponents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlCommonComponentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlCommonComponentsApplication.class, args);
+	}
+
+}
